950dcaca1d091245435ed530179f32f3e513239e
Florin <48189025+FlorinInfo@users.noreply.github.com>
2024-01-02 16:08:56 +0200

    sync

--- bumo.tab.h
+++ bumo.tab.h
@@ -1,14 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
-   Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,78 +33,93 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
-
-#ifndef YY_YY_BUMO_TAB_H_INCLUDED
-# define YY_YY_BUMO_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    ASSIGN = 258,
-    PROGR = 259,
-    BGIN = 260,
-    END = 261,
-    CONST = 262,
-    FUNCTION = 263,
-    ADD = 264,
-    AND = 265,
-    NOT = 266,
-    OR = 267,
-    IF = 268,
-    ELSE = 269,
-    THEN = 270,
-    EQ = 271,
-    NQ = 272,
-    GT = 273,
-    LT = 274,
-    LE = 275,
-    GE = 276,
-    IDENTIFIER = 277,
-    TYPE = 278,
-    STRING_VALUE = 279,
-    CHAR_VALUE = 280,
-    BOOL_VALUE = 281,
-    INT_VALUE = 282,
-    REAL_VALUE = 283,
-    SUB = 284,
-    MUL = 285,
-    DIV = 286
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ASSIGN = 258,
+     PROGR = 259,
+     BGIN = 260,
+     END = 261,
+     CONST = 262,
+     FUNCTION = 263,
+     ADD = 264,
+     AND = 265,
+     NOT = 266,
+     OR = 267,
+     IF = 268,
+     ELSE = 269,
+     THEN = 270,
+     EQ = 271,
+     NQ = 272,
+     GT = 273,
+     LT = 274,
+     LE = 275,
+     GE = 276,
+     IDENTIFIER = 277,
+     TYPE = 278,
+     STRING_VALUE = 279,
+     CHAR_VALUE = 280,
+     BOOL_VALUE = 281,
+     INT_VALUE = 282,
+     REAL_VALUE = 283,
+     SUB = 284,
+     DIV = 285,
+     MUL = 286
+   };
 #endif
+/* Tokens.  */
+#define ASSIGN 258
+#define PROGR 259
+#define BGIN 260
+#define END 261
+#define CONST 262
+#define FUNCTION 263
+#define ADD 264
+#define AND 265
+#define NOT 266
+#define OR 267
+#define IF 268
+#define ELSE 269
+#define THEN 270
+#define EQ 271
+#define NQ 272
+#define GT 273
+#define LT 274
+#define LE 275
+#define GE 276
+#define IDENTIFIER 277
+#define TYPE 278
+#define STRING_VALUE 279
+#define CHAR_VALUE 280
+#define BOOL_VALUE 281
+#define INT_VALUE 282
+#define REAL_VALUE 283
+#define SUB 284
+#define DIV 285
+#define MUL 286
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-union YYSTYPE
+typedef union YYSTYPE
+#line 22 "bumo.y"
 {
-#line 17 "bumo.y"
-
     char* str;
     int intval;
     float ftval;
     bool bval;
-
-#line 96 "bumo.tab.h"
-
-};
-typedef union YYSTYPE YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+}
+/* Line 1529 of yacc.c.  */
+#line 118 "bumo.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_BUMO_TAB_H_INCLUDED  */
